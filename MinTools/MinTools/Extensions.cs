deea4991e2df8ffa8b2453bda02b4cdc9d4f0ec7
Sascha Ernst <sascha.m.ernst@gmail.com>
2017-02-28 14:10:27 +0100

    initial commit

---
+++ MinTools/MinTools/Extensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinMVC
+{
+	public static class Extensions
+	{
+		public static void Each<T>(this IEnumerable<T> enumerable, Action<T> handler)
+		{
+			foreach (T item in enumerable)
+			{
+				handler(item);
+			}
+		}
+
+		public static TValue Retrieve<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			return Retrieve(dictionary, key, Activator.CreateInstance<TValue>);
+		}
+
+		public static TValue Retrieve<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defFunc)
+		{
+			TValue value;
+
+			if (!dictionary.TryGetValue(key, out value))
+			{
+				dictionary[key] = value = defFunc();
+			}
+
+			return value;
+		}
+
+		public static bool AddNewEntry<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			bool isKeyNew = !dictionary.ContainsKey(key);
+
+			if (isKeyNew)
+			{
+				dictionary[key] = value;
+			}
+
+			return isKeyNew;
+		}
+
+		public static bool UpdateEntry<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+		{
+			bool isKeyExisting = dictionary.ContainsKey(key);
+
+			if (isKeyExisting)
+			{
+				dictionary[key] = value;
+			}
+
+			return isKeyExisting;
+		}
+
+		public static TValue Withdraw<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			TValue value;
+
+			if (dictionary.TryGetValue(key, out value))
+			{
+				dictionary.Remove(key);
+			}
+
+			return value;
+		}
+
+		public static bool IsEmpty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
+
+		public static T Pop<T>(this IList<T> list)
+		{
+			T item = list[0];
+			list.RemoveAt(0);
+
+			return item;
+		}
+	}
+}
---
+++ MinTools/MinTools/MinSignal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinMVC
+{
+	public interface IMinSignal
+	{
+		void Reset ();
+	}
+
+	public class MinSignal : IMinSignal
+	{
+		readonly HashSet<Action> listeners = new HashSet<Action>();
+
+		public bool Add (Action listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (Action listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call ()
+		{
+			foreach (var listener in listeners) {
+				listener();
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+
+	public class MinSignal<T> : IMinSignal
+	{
+		readonly HashSet<Action<T>> listeners = new HashSet<Action<T>>();
+
+		public bool Add (Action<T> listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (Action<T> listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call (T p1)
+		{
+			foreach (var listener in listeners) {
+				listener(p1);
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+
+	public class MinSignal<T, U> : IMinSignal
+	{
+		readonly HashSet<Action<T, U>> listeners = new HashSet<Action<T, U>>();
+
+		public bool Add (Action<T, U> listener)
+		{
+			return listeners.Add(listener);
+		}
+
+		public bool Remove (Action<T, U> listener)
+		{
+			return listeners.Remove(listener);
+		}
+
+		public void Call (T p1, U p2)
+		{
+			foreach (var listener in listeners) {
+				listener(p1, p2);
+			}
+		}
+
+		public void Reset ()
+		{
+			listeners.Clear();
+		}
+	}
+}
